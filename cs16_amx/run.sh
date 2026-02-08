ce200ed9d61a780d8a25a8bcacfa57e771e88364
z0mbyHG <djaha100@gmail.com>
2026-02-08 03:40:16 +0100

    Enhance run script for CS16 server setup
    Added configuration and setup for Counter-Strike 1.6 server.

--- cs16_amx/run.sh
+++ cs16_amx/run.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-cd /hlds
+GAME_ROOT=/hlds
+GAME_CSTRIKE=${GAME_ROOT}/cstrike
+CONFIG_ROOT=/config/cs16
+CONFIG_CSTRIKE=${CONFIG_ROOT}/cstrike
+
+mkdir -p ${CONFIG_ROOT}
+
+# Prvi start: kopiraj kompletan cstrike u /config
+if [ ! -d "${CONFIG_CSTRIKE}" ]; then
+  echo "[CS16] First start detected – copying cstrike to /config"
+  cp -a ${GAME_CSTRIKE} ${CONFIG_CSTRIKE}
+fi
+
+# Zamijeni originalni cstrike symlinkom
+rm -rf ${GAME_CSTRIKE}
+ln -s ${CONFIG_CSTRIKE} ${GAME_CSTRIKE}
+
+# Safety files
+touch ${GAME_CSTRIKE}/listip.cfg
+touch ${GAME_CSTRIKE}/banned.cfg
+
+cd ${GAME_ROOT}
 
 ./hlds_run \
  -game cstrike \
